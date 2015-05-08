8730856c4a47af7d839834f485a4b77c3eca7ca0
Finnegan Hewitt <fbhewitt@gmail.com>
2015-05-08 14:53:10 -0600

    Ex 01 done

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,41 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
-  }
+    if (string1.length === string2.length)
+      return true
+    else
+      return false
+  },
+  firstGreater: function(one, two){
+    if (one >  two)
+    return true
+  else
+    return false
+  },
+  firstGreaterThanOrEqualTo: function(one, two){
+    if (one >= two)
+    return true
+  else
+    return false
+  },
+  firstLengthGreater: function(one, two){
+    if (one === String){
+      if (one.split("").length > two.split("").length)
+        return true
+      else
+        return false
+    }
+    else if (one.length > two.length)
+      return true
+    else
+      return false
+  },
+  secondGreater: function(one, two){
+    if (one <  two)
+    return true
+  else
+    return false
+  },
+
+
+
 }
